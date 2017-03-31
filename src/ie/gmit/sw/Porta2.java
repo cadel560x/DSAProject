265a2f26c4236c36ce7fb9b2c1f53e7c6a615b9f
G00329649 <G00329649@CR313.gmadtree.gmit.ie>
2017-03-31 17:08:06 +0100

    Porta cipher using 'HashMap'

---
+++ src/ie/gmit/sw/Porta2.java
@@ -0,0 +1,125 @@
+package ie.gmit.sw;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Porta2 {
+//	Member attributes/fields
+	private Map<Character, Map<Character, Character> > tableau;
+//	private List< Map< Character, Character > > tableArray;
+	private final int NUMBER_ENCONDING_TABLES = 13;
+//	I explicity declare 13 encoding tables because I need their explicit references.
+//	I use their references/names to fill up the 'tableau'
+//	private Map< Character, Character > encodeTable1 = new HashMap<>();
+//	private Map< Character, Character > encodeTable2 = new HashMap<>();
+//	private Map< Character, Character > encodeTable3 = new HashMap<>();
+//	private Map< Character, Character > encodeTable4 = new HashMap<>();
+//	private Map< Character, Character > encodeTable5 = new HashMap<>();
+//	private Map< Character, Character > encodeTable6 = new HashMap<>();
+//	private Map< Character, Character > encodeTable7 = new HashMap<>();
+//	private Map< Character, Character > encodeTable8 = new HashMap<>();
+//	private Map< Character, Character > encodeTable9 = new HashMap<>();
+//	private Map< Character, Character > encodeTable10 = new HashMap<>();
+//	private Map< Character, Character > encodeTable11 = new HashMap<>();
+//	private Map< Character, Character > encodeTable12 = new HashMap<>();
+//	private Map< Character, Character > encodeTable13 = new HashMap<>();
+	private String key;
+	
+	
+	
+	
+//	Constructors
+	public Porta2() {
+		key = "PORTA";
+	}
+
+	public Porta2(String key) {
+		this.key = key; // check out if 'toUpperCase' uses loops, there is a chance to use a map...
+	}
+
+	
+	
+
+//	Getters & setters
+	public String getKey() {
+		return key;
+	} // to delete, not a good idea to get the key ;-)
+
+	public void setKey(String key) {
+//		Watch out for those pranksters that change the key to a invalid value
+		if (key == null || key.equals("")) {
+			System.out.println("The key can't be 'null' or empty!\nUsing 'PORTA' as key");
+			this.key = "PORTA";
+		} else
+			this.key = key;
+	}
+
+
+	
+	
+//	Other methods
+	public void init() {
+		tableau = new HashMap<>();
+		
+		List< Map< Character, Character > > tableArray = new ArrayList<>();
+		Map< Character, Character> tempEncondigTable;
+		char keyChar;
+		char tempEncChar;
+		
+		for (int i = 0 ; i < NUMBER_ENCONDING_TABLES; i += 2) {
+			tempEncondigTable = new HashMap<>();
+//			tempChar = (char) ('A' + i);
+			for (char character = 'A'; character < '['; character++) {
+				keyChar = (char)('A' + i);
+				tempEncChar = processChar(keyChar, character);
+				tempEncondigTable.put(character, tempEncChar);
+//				tempEncondigTable.put(character, processChar((char)('A' + i), character));
+			}
+			
+			tableArray.add(tempEncondigTable);
+			
+//			key = i 
+		}
+
+	}
+	
+	private char processChar(char keyCharacter,char plainCharacter) // Comment this method
+	{
+		int keyCharacterPosition=(keyCharacter-'A')/2;
+		int plainCharacterPosition=plainCharacter-'A';
+		if (plainCharacter - 'M' > 0)
+		{
+			
+			return (char)('A' + (plainCharacterPosition - keyCharacterPosition)%13);
+		}
+		else
+		{			
+			return (char)((plainCharacterPosition + keyCharacterPosition)%13 + 'N');
+		}
+	} // processChar	
+	
+	
+	public String encode(String word) {
+		StringBuilder sb = new StringBuilder();
+		char codedChar;
+		int keyLength = key.length();
+		
+		for (int i = 0; i < word.length(); i++) {
+//			'i % keyLength' avoids 'IndexOutOfBoundsException', no matter the length of the word to process
+			codedChar = processChar(key.charAt(i % keyLength),  word.toUpperCase().charAt(i));
+			sb.append(codedChar);
+		}
+		
+		return sb.toString();
+		
+	} // enconde
+
+	
+	public String decode(String cypherText) {
+		return encode(cypherText);
+		
+	} // decode
+	
+}
